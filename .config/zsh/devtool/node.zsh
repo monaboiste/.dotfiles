35ed79f9818a30ee6165400f5dc3432c6f7ebbb2
monaboiste <monikaszumna4@gmail.com>
2025-12-09 19:02:36 +0100

    chore(node): optimize zsh startup - node lazy loading

--- .config/zsh/devtool/node.zsh
+++ .config/zsh/devtool/node.zsh
@@ -1,9 +1,20 @@
 #!/usr/bin/zsh
 
 export NVM_DIR="${XDG_BIN_HOME:-$HOME/.nvm}/nvm"
-source $(brew --prefix nvm)/nvm.sh
 
-if ! (( $+commands[npm] )); then
+_load_nvm() {
+  unset -f nvm npm npx node _load_nvm
+  source "$(brew --prefix nvm)/nvm.sh"
+}
+
+nvm()  { _load_nvm; nvm  "$@"; }
+npm()  { _load_nvm; npm  "$@"; }
+npx()  { _load_nvm; npx  "$@"; }
+node() { _load_nvm; node "$@"; }
+
+local node_versions=("$NVM_DIR"/versions/node/*(N))
+if (( ${#node_versions} == 0 )); then
   echo "Installing latest Node.js LTS via nvm..."
+  _load_nvm
   nvm install --lts >/dev/null
 fi
