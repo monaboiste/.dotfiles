f48602d23c245428b56c4951110746ee21c54c33
monaboiste <monikaszumna4@gmail.com>
2026-01-21 20:01:36 +0100

    chore(nvim): move colorscheme configuration to plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,10 +9,4 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 
 -- Color scheme
-require("catppuccin").setup {
-  color_overrides = {
-    all = { base = "#23232b" }
-  }
-}
-
 vim.cmd.colorscheme "catppuccin"
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,13 @@
-return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-}
+return {{
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    color_overrides = {
+      all = {
+        base = "#23232b"
+      }
+    }
+  }
+}}
