9fdf90e6c1f17747f13d8a6fbe609849f9e17552
monaboiste <monikaszumna4@gmail.com>
2026-01-21 21:16:12 +0100

    feat(nvim): use telescope to find files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,3 +10,16 @@ vim.cmd("set number")
 
 -- Color scheme
 vim.cmd.colorscheme "catppuccin"
+
+-- Navigation
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>f", builtin.find_files, {
+  desc = "Find files"
+})
+vim.keymap.set("n", "<C-S>", builtin.live_grep, {
+  desc = "Live grep"
+ })
+vim.keymap.set("n", "<leader>b", builtin.buffers, {
+  desc = "Buffers"
+ })
+
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,13 +1,27 @@
-return {{
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    color_overrides = {
-      all = {
-        base = "#23232b"
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      color_overrides = {
+        all = {
+          base = "#23232b"
+        }
+      }
+    }
+  }, 
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = 
+    {
+      'nvim-lua/plenary.nvim', 
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make'
       }
     }
   }
-}}
+}
