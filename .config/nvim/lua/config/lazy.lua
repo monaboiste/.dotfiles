a625b371fb21f6930173efc9e0ec3a16e991ac79
monaboiste <monikaszumna4@gmail.com>
2026-01-22 21:05:33 +0100

    feat(nvim): disable notofications

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,5 +24,11 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
-  checker = { enabled = true },
+  checker = { 
+    enabled = true,
+    notify = false
+  },
+  change_detection = { 
+    notify = false 
+  }
 })
