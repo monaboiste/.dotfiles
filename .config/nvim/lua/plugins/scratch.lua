b8f6d5168fcee1cedc42f78f0c56bbc01d28a383
monaboiste <monikaszumna4@gmail.com>
2026-01-29 15:07:52 +0100

    fix(nvim): add missing scratch deps

--- .config/nvim/lua/plugins/scratch.lua
+++ .config/nvim/lua/plugins/scratch.lua
@@ -2,6 +2,9 @@ return {
   {
     "LintaoAmons/scratch.nvim",
     event = "VeryLazy",
+    dependencies = {
+      "ibhagwan/fzf-lua"
+    },
     keys = {
       { "<leader>sn", "<cmd>Scratch<cr>",     desc = "New scratch file" },
       { "<leader>so", "<cmd>ScratchOpen<cr>", desc = "Open scratch file" },
