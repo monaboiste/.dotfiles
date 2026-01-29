68fe2a75609ab9bf4958941165e43cdb4dc0ce3e
monaboiste <monikaszumna4@gmail.com>
2026-01-29 13:45:27 +0100

    feat(nvim): scratch files

---
+++ .config/nvim/lua/plugins/scratch.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "LintaoAmons/scratch.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>sn", "<cmd>Scratch<cr>",     desc = "New scratch file" },
+      { "<leader>so", "<cmd>ScratchOpen<cr>", desc = "Open scratch file" },
+    }
+  }
+}
