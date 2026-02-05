8aa1d97d104a2119d5470547883e8587000404dd
monaboiste <monikaszumna4@gmail.com>
2026-02-05 21:49:10 +0100

    feat(nvim): add trouble

--- .config/nvim/lua/ui/interface.lua
+++ .config/nvim/lua/ui/interface.lua
@@ -3,6 +3,7 @@ vim.opt.winborder = "rounded"
 vim.pack.add({
   "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/ibhagwan/fzf-lua",
+  "https://github.com/folke/trouble.nvim",
   "https://github.com/folke/which-key.nvim",
 })
 
@@ -33,6 +34,54 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fc", fzf.commands, { desc = "Find Command" })
 
+-- Trouble
+
+require("trouble").setup({
+  auto_close = true,
+  restore_window = false,
+})
+
+vim.keymap.set(
+  "n",
+  "<leader>xx",
+  "<cmd>Trouble diagnostics toggle<cr>",
+  { desc = "Diagnostics" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>xX",
+  "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+  { desc = "Buffer Diagnostics" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>cs",
+  "<cmd>Trouble symbols toggle focus=false<cr>",
+  { desc = "Symbols" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>cl",
+  "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+  { desc = "LSP Definitions/refs" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>xL",
+  "<cmd>Trouble loclist toggle<cr>",
+  { desc = "Location List" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>xQ",
+  "<cmd>Trouble qflist toggle<cr>",
+  { desc = "Quickfix List" }
+)
+
+local config = require("fzf-lua.config")
+local actions = require("trouble.sources.fzf").actions
+config.defaults.actions.files["ctrl-t"] = actions.open -- in fzf-lua hit <c-t> to open trouble
+
 -- "What to press next" overlay
 local wk = require("which-key")
 wk.setup({ preset = "helix" })
@@ -42,7 +91,9 @@ vim.keymap.set("n", "<leader>?", function()
 end, { desc = "Buffer Local Keymaps (which-key)" })
 
 wk.add({
-  { "<leader>f", group = "Find" },
-  { "<leader>w", group = "Windows" },
   { "<leader>c", group = "Code (LSP)" },
+  { "<leader>f", group = "Find" },
+  { "<leader>g", group = "Goto" },
+  { "<leader>w", group = "Windows" },
+  { "<leader>x", group = "Trouble" },
 })
