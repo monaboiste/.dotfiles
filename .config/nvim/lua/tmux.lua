019daa8c218eaad00359ad93b8b6fb3f04bacd67
monaboiste <monikaszumna4@gmail.com>
2026-02-05 22:28:57 +0100

    feat(nvim): add new tab keymap

--- .config/nvim/lua/tmux.lua
+++ .config/nvim/lua/tmux.lua
@@ -1,7 +1,8 @@
 -- Panes navigation to better fit tmux convention
 vim.keymap.set("n", '<leader>w"', ":split<cr>", { desc = "Split horizontally" })
-vim.keymap.set("n", '<leader>w%', ":vsplit<cr>", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>w%", ":vsplit<cr>", { desc = "Split vertically" })
 vim.keymap.set("n", "<leader>wq", ":quit<cr>", { desc = "Close" })
+vim.keymap.set("n", "<leader>t", ":tabnew<cr>", { desc = "New Tab" })
 
 -- Tmux
 vim.pack.add({ "https://github.com/christoomey/vim-tmux-navigator" })
@@ -9,4 +10,3 @@ vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<cr>")
 vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<cr>")
 vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<cr>")
 vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<cr>")
-
