4d19a9357da648140fa95eede67f5235a85f87ff
monaboiste <monikaszumna4@gmail.com>
2026-02-08 16:09:55 +0100

    chore(nvim): statusline adjustments

--- .config/nvim/lua/ui/styling.lua
+++ .config/nvim/lua/ui/styling.lua
@@ -5,12 +5,15 @@ vim.opt.listchars = {
   tab = "->",
 }
 
+-- Hide command line when inactive
+vim.opt.cmdheight = 0
+
 -- Theme
 local bg_color_override = "#23232b"
 
 vim.pack.add({
   "https://github.com/catppuccin/nvim",
-  "https://github.com/nvim-lualine/lualine.nvim"
+  "https://github.com/nvim-lualine/lualine.nvim",
 })
 
 require("catppuccin").setup({
@@ -19,7 +22,7 @@ require("catppuccin").setup({
     all = {
       base = bg_color_override,
       mantle = bg_color_override,
-      crust = bg_color_override
+      crust = bg_color_override,
     },
   },
   integrations = { lualine = { enabled = true } },
@@ -30,36 +33,55 @@ vim.cmd.colorscheme("catppuccin-mocha")
 require("lualine").setup({
   options = {
     theme = "catppuccin",
-    section_separators = "",
-    component_separators = "",
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
     globalstatus = true,
   },
   winbar = {
     lualine_a = {
-      { "filename", color = { fg = "grey", bg = "none" } }
+      { "filename", color = { fg = "grey", bg = "none" } },
     },
   },
   sections = {
     lualine_a = {
-      "mode",
+      { -- Empty element to align itself with tmux statusline
+        function()
+          return " "
+        end,
+        padding = { left = 0, right = 0 },
+        color = { bg = "NONE" },
+      },
+      { "mode" },
       {
         function()
           local reg = vim.fn.reg_recording()
-          if reg == "" then return "" end
+          if reg == "" then
+            return ""
+          end
           return "⏺ " .. reg
         end,
-        cond = function() return vim.fn.reg_recording() ~= "" end,
-        color = { fg = "#23232b", gui = "bold" }
-      }
+        cond = function()
+          return vim.fn.reg_recording() ~= ""
+        end,
+        color = { fg = "#23232b", gui = "bold" },
+      },
     },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = {},
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
-    lualine_z = { "location" }
+    lualine_z = {
+      { "location" },
+      { -- Empty element to align itself with tmux statusline
+        function()
+          return " "
+        end,
+        padding = { left = 0, right = 0 },
+        color = { bg = "NONE" },
+      },
+    },
   },
   inactive_winbar = {
-    lualine_c = { "filename" }
-  }
+    lualine_c = { "filename" },
+  },
 })
-
