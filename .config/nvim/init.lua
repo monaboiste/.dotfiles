3c218e2c7a4cfc724a31f9ce82d5abeb442124ac
monaboiste <monikaszumna4@gmail.com>
2026-07-16 12:48:49 +0200

    feat(nvim): copy content to clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,7 @@ vim.o.scrolloff = 16 -- keep cursor away from the screen edge
 vim.opt.undofile = true -- persistent undo history across sessions
 vim.opt.swapfile = false
 vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus" -- copy to clipboard
 vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]]) -- pasting over a selection no longer clobbers the clipboard
 
 --
